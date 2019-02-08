4efb2c72c408534ca0921167a4e103f5f1dc8f54
Kurapati Midhun Kumar <S531502@nwmissouri.edu>
2019-02-08 14:34:07 -0600

    Added a new unit tests

--- tests/UnitTests/UnitTest1.cs
+++ tests/UnitTests/UnitTest1.cs
@@ -6,9 +6,44 @@ namespace UnitTests
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        public void Addition_WithTwoInts()
         {
-
+            var expected = 10;
+            var a = 8;
+            var b = 2;
+            var actual = Addition(a, b);
+            Assert.Equal(expected, actual);
         }
+
+        [Fact]
+        public void Addition_WithTwoNegativeInts()
+        {
+            var expected = -15;
+            var a = -9;
+            var b = -6;
+            var actual = Addition(a, b);
+            Assert.Equal(expected, actual);
+        }
+
+
+
+
+        [Theory]
+        [InlineData(3, 3, 6)]
+        [InlineData(-6, -2, -8)]
+        [InlineData(-8, 2, -6)]
+        public void Addition_WithDifferentInputs(int n1, int n2, int value)
+        {
+            Assert.Equal(Addition(n1, n2), value);
+        }
+
+
+
+        int Addition(int x, int y)
+        {
+            return x + y;
+        }
+
+
     }
 }
